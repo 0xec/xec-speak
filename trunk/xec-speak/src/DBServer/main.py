e11890ff72c535608d34274a19ab29121ee3e163
tbxyww <tbxyww@f213a425-9ee1-ac9b-1843-d01199319b06>
2011-04-26 04:55:10 +0000

    加入线程同步

--- trunk/xec-speak/src/DBServer/main.py
+++ trunk/xec-speak/src/DBServer/main.py
@@ -9,18 +9,56 @@ from common.xec_tcpsvr import *
 from common.logger import *
 
 
-config_file = 'config.conf'
-
-# code
-
 listen_host = ''
 listen_port = 0
-server = []
 
 class DatabaseServer(SocketServer.StreamRequestHandler):
-    pass
+    def __init__(self, request, client_address, server):
+        self.timeout = 2
+        self.disable_nagle_algorithm = True
+        SocketServer.StreamRequestHandler.__init__(self, request, client_address, server)
+        
+    def handle(self):
+            
+        try:
+            # verifier version
+            if not self.auth_version():
+                return
+            
+            logger(__name__, 'request accept : %s:%d' % (self.client_address[0], self.client_address[1]))
+                
+            # 验证账户密码
+            usr = self.rfile.read(32)
+            pwd = self.rfile.read(32)
+            if not self.auth_logonuser(usr, pwd):
+                return
+            
+            # make session key
+            
+            # put sesssion key to main server
+            
+            # send session, chat server to client
+           
+            logger(__name__, 'requect process finish')
+
+        except Exception, err:
+            self.request.close()
+            logger(__name__, str(err).decode('gbk'))
+                
+        logger(__name__, 'requect close')
+            
+    def finish(self):
+        logger(__name__, 'client disconnect...')
+
+def main():
+    logger(__name__, 'Logon Server Starting....')
+    
+    # 读取配置文件
+    listen_host = read_conf_file('config', 'host')
+    listen_port = int(read_conf_file('config', 'port'))
+    
+    start_listen_thread(DatabaseServer, listen_host, listen_port)
+    logger(__name__, 'Logon Server Exit.')
 
 if __name__ == '__main__':
-    logger('Logon Server Starting....')
-    start_listen_thread(DatabaseServer, '', 8000)
-    logger('Logon Server Exit.')
+    main()
