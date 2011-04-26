8284abd2f25d968fc46e6b40bb2d56423dcffbc3
tbxyww <tbxyww@f213a425-9ee1-ac9b-1843-d01199319b06>
2011-04-26 07:19:32 +0000

    完成登录验证

--- trunk/xec-speak/src/LogonServer/svr-test.py
+++ trunk/xec-speak/src/LogonServer/svr-test.py
@@ -4,9 +4,9 @@
 from socket import *
 
 if __name__ == '__main__':
-    for i in range(1, 9999):
-        usr = '%-32s' % ('xx')
-        pwd = '%-32s' % ('yy')
+    #for i in range(1, 9999):
+        usr = '%-32s' % ('admin')
+        pwd = '%-32s' % ('admin888')
         data = '\x00\x01' + usr + pwd
         sockobj = socket(AF_INET, SOCK_STREAM)
         sockobj.connect(('localhost', 8400) )
