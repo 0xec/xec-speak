075ff70276f78b0b67c388e3ff68396dda14b2a4
tbxyww <tbxyww@f213a425-9ee1-ac9b-1843-d01199319b06>
2011-04-27 11:17:36 +0000

    完成大厅服务器

--- xec-speak/src/client-test/svr-test.py
+++ xec-speak/src/client-test/svr-test.py
@@ -19,11 +19,25 @@ if __name__ == '__main__':
         s = socket(AF_INET, SOCK_STREAM)
         s.connect(('localhost', 8400) )
         s.send(data)
-        sess = s.recv(1024)
-        print sess
-#        cmd         = sess[:5].strip()
-#        session_key = sess[5:37].strip()
-#        hall_host   = sess[37:70].strip()
-#        hall_port   = int(sess[70:75].strip())
-        
+        data = s.recv(1024)
         s.close
+        rep = json_dec.decode(data)
+        print rep
+        
+        info = {}
+        
+        s = None
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((rep['HallHost'], rep['HallPort']))
+        
+        info['Request'] = 'ChatRoomList'
+        data = json_enc.encode(info)
+        s.send(data)
+        data = s.recv(1024)
+        s.close
+        rep = json_dec.decode(data)
+        for label in rep:
+            print label
+            print rep[label]
+        print rep
+        
